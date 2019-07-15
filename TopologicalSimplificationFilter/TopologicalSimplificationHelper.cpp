646312f38b326b913f19e072f1fa3de00af308c5
Ian <ixgauth@yahoo.com>
2019-07-15 00:58:42 -0400

    Actually adding the files which I forgot to do at first.

---
+++ TopologicalSimplificationFilter/TopologicalSimplificationHelper.cpp
@@ -0,0 +1,24 @@
+#include                  <TopologicalSimplificationHelper.h>
+
+using namespace std;
+using namespace ttk;
+
+TopologicalSimplificationHelper::TopologicalSimplificationHelper():
+  triangulation_{},
+  vertexNumber_{},
+  constraintNumber_{},
+  inputScalarFieldPointer_{},
+  vertexIdentifierScalarFieldPointer_{},
+  inputOffsetScalarFieldPointer_{},
+  considerIdentifierAsBlackList_{},
+  outputScalarFieldPointer_{},
+  outputOffsetScalarFieldPointer_{},
+  helperScalarFieldPointer_{},
+  helperOffsetScalarFieldPointer_{}
+{
+  considerIdentifierAsBlackList_ = false;
+}
+
+TopologicalSimplificationHelper::~TopologicalSimplificationHelper(){
+}
+
